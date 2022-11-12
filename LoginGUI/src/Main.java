23513d0adb7c608e4ad5fe59a87a8526a54d5c17
Rishika Srinivas <rishikasrinivas@Rishikas-MacBook-Pro.local>
2022-11-12 09:11:05 -0800

    first try commit

--- LoginGUI/src/Main.java
+++ LoginGUI/src/Main.java
@@ -6,14 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Login l = new Login("",len,len,null);
-		/*CreateNewContact s = new CreateNewContact(null);
-		ArrayList<String> u = new ArrayList<String>();
-		u.add("Rishika, Srinivas");
-		u.add("Neela, Sr");
-		u.add("Srinivas, Kum");
-		u.add("Cat, ");
-		s.sort(u);
-		s.writeToFile(u);*/
+		
 		
 	}
 
