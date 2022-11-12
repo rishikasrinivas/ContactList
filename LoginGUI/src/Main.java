c4f29f8d7895d23da5d795ce858bd0d5eef69116
Rishika Srinivas <rishikasrinivas@Rishikas-MacBook-Pro.local>
2022-11-12 09:14:49 -0800

    removed comment

--- LoginGUI/src/Main.java
+++ LoginGUI/src/Main.java
@@ -4,7 +4,6 @@ import java.util.Set;
 public class Main {
 	static int len = 700;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Login l = new Login("",len,len,null);
 		
 		
